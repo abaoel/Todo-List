f524cb8eea4e750baf7f3ebd51809bef99e88591
Emmanuel Abao <46640200+abaoel@users.noreply.github.com>
2026-04-29 11:37:37 +0800

    Update db-test.js

--- db-test.js
+++ db-test.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const uriDirect = "mongodb://abaoel_db_user:NfLF8wKuStDFfLwN@ac-wtkgi3j-shard-00-00.owtte45.mongodb.net:27017,ac-wtkgi3j-shard-00-01.owtte45.mongodb.net:27017,ac-wtkgi3j-shard-00-02.owtte45.mongodb.net:27017/job-board?tls=true&replicaSet=atlas-comqsm-shard-0&authSource=admin&retryWrites=true&w=majority";
+const uriDirect = "";
 
 async function testUri() {
   const client = new MongoClient(uriDirect, {
